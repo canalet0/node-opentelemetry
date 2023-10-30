d11795897e6420618755af7e2e71908e80e44c64
canalet0 <roc0canal@gmail.com>
2023-10-30 14:53:16 -0300

    Adding Jaeger as tracing exporter

--- src/infrastructure/instrumentation.js
+++ src/infrastructure/instrumentation.js
@@ -1,29 +1,36 @@
 /*instrumentation.js*/
 // Require dependencies
 const { NodeSDK } = require('@opentelemetry/sdk-node');
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
 const {
   getNodeAutoInstrumentations,
 } = require('@opentelemetry/auto-instrumentations-node');
-const {
-  PeriodicExportingMetricReader,
-  ConsoleMetricExporter,
-} = require('@opentelemetry/sdk-metrics');
 const { Resource } = require('@opentelemetry/resources');
-const {
-  SemanticResourceAttributes,
-} = require('@opentelemetry/semantic-conventions');
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
 
+//exporters
+const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
+const { ConsoleMetricExporter } = require('@opentelemetry/sdk-metrics');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-proto');
+const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-proto');
+
+//readers
+const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
 
 const sdk = new NodeSDK({
   resource: new Resource({
     [SemanticResourceAttributes.SERVICE_NAME]: 'node-opentelemetry',
     [SemanticResourceAttributes.SERVICE_VERSION]: '0.1.0',
   }),
-  traceExporter: new ConsoleSpanExporter(),
-  metricReader: new PeriodicExportingMetricReader({
-    exporter: new ConsoleMetricExporter(),
-  }),
+  traceExporter: //[ 
+    //new ConsoleSpanExporter(),
+    new OTLPTraceExporter()
+  //]
+  ,
+  metricReader: //[
+   // new PeriodicExportingMetricReader({exporter: new ConsoleMetricExporter(),}),
+    new PeriodicExportingMetricReader({exporter: new OTLPMetricExporter(),})
+  //],
+  ,
   instrumentations: [getNodeAutoInstrumentations()],
 });
 
