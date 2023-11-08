aec85ecd3af8acb79855e91fc3f6774857cd8dda
canalet0 <roc0canal@gmail.com>
2023-11-08 15:41:08 -0300

    Removed manual span

--- src/api/dice.js
+++ src/api/dice.js
@@ -1,26 +1,21 @@
 /*dice.js*/
 
 const Logger = require('../cross-cutting/logger.js');
-const { tracer } = require('../utils/tracer.js');
+const DICE_MIN_VALUE = 1;
+const DICE_MAX_VALUE = 6;
 
 
 function rollOnce(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-function rollTheDice(rolls, min, max) {
-    // Create a span. A span must be closed.
-    return tracer.startActiveSpan('rollTheDiceActiveSpan', (span) => {
-      let logger = new Logger();
-      logger.info('log INFO inside rollTheDiceActiveSpan');
-      const result = [];
-      for (let i = 0; i < rolls; i++) {
-        result.push(rollOnce(min, max));
-      }
-      // Be sure to end the span!
-      span.end();
-      return result;
-    });
-  }
+function rollTheDice(rolls) {
+    new Logger().info('Received request to roll dice ' + rolls + ' times');
+    const result = [];
+    for (let i = 0; i < rolls; i++) {
+      result.push(rollOnce(DICE_MIN_VALUE, DICE_MAX_VALUE));
+    }
+    return result;
+}
 
 module.exports = { rollTheDice };
